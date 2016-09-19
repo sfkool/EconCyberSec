53b30d256eb6fa2552282b2978c6171a009b18a6
Vishan Baldew <VBaldew@deloitte.nl>
2016-09-19 23:12:49 +0200

    Add some code

--- R code/Assignment2.R
+++ R code/Assignment2.R
@@ -1,9 +1,22 @@
 #install.packages('psych')
 library(psych)
 
+##Read in CSV file
 RealDeal <- read.csv("C:\\Users\\vbaldew\\Python\\EconCybSec\\therealdeal_2016_04_20_17_21_45.csv")
 
-colnames(RealDeal) <- c('Date','Product','URL','Price','User','Reviews','Product')
+##Add columnnames
+colnames(RealDeal) <- c('Date','Description','URL','Price','User','Reviews','Product')
 
-describe(RealDeal)
+##Get insight into the data
 summary(RealDeal)
+
+##Remove rows with no 'BTC' for price
+#Text to select on
+currency <- "BTC "
+
+#Clean dataframe based on rows that conain prices that start with "BTC"
+RealDeal_Clean <- RealDeal[grep(currency,RealDeal$Price),]
+
+Unique_Products <- unique(RealDeal_Clean$Product)
+
+
